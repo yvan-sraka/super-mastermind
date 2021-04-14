3a53ae5562186add82317dafb9048fa789bf7520
masatakaIshii <mishii1@myges.fr>
2021-04-15 01:13:55 +0200

    level 7 done

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
+use ansi_term::Colour::{Blue, Cyan, Green, Purple, Red, White, Yellow, RGB};
+use ansi_term::{ANSIString, ANSIStrings};
 use std::fmt;
 use std::vec;
 
 #[derive(Debug)]
 enum Color {
     White,
-    Black,
+    Orange,
     Blue,
     Green,
     Red,
@@ -19,29 +21,39 @@ impl fmt::Display for Color {
     }
 }
 
+fn color_first_color_letter(color: &Color) -> ANSIString {
+    let first_letter = color.to_string().chars().next().unwrap().to_string();
+    return match *color {
+        Color::Blue => Blue.paint(first_letter),
+        Color::Orange => RGB(255, 165, 0).paint(first_letter),
+        Color::Cyan => Cyan.paint(first_letter),
+        Color::Green => Green.paint(first_letter),
+        Color::Purple => Purple.paint(first_letter),
+        Color::Red => Red.paint(first_letter),
+        Color::White => White.paint(first_letter),
+        Color::Yellow => Yellow.paint(first_letter),
+    };
+}
+
 fn fancy_print_guess(guess: &[Color]) {
-    let mut vec_colors: Vec<char> = Vec::new();
+    let mut vec_colors: Vec<ANSIString> = Vec::new();
 
     for color in guess {
-        vec_colors.push(color.to_string().chars().next().unwrap());
+        vec_colors.push(color_first_color_letter(color));
     }
-    let color_str: String = vec_colors.into_iter().collect();
-    println!("{:?}", color_str);
+    println!("{}", ANSIStrings(&vec_colors));
 }
 
 fn main() {
     let guess = vec![
-        Color::Black,
+        Color::Orange,
         Color::White,
-        Color::Black,
-        Color::Green,
-        Color::Blue,
-    ];
-    fancy_print_guess(&[
         Color::Red,
-        Color::Red,
-        Color::Blue,
         Color::Yellow,
         Color::Green,
-    ]);
+        Color::Blue,
+        Color::Cyan,
+        Color::Purple,
+    ];
+    fancy_print_guess(&guess);
 }

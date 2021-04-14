e7c3a1cf2c40909c6b2f35165b2b252461030743
masatakaIshii <mishii1@myges.fr>
2021-04-14 23:27:10 +0200

    level 5 done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use std::fmt;
+use std::vec;
+
+#[derive(Debug)]
+enum Color {
+    White,
+    Black,
+    Blue,
+    Green,
+    Red,
+    Yellow,
+    Purple,
+    Cyan,
+}
+
+impl fmt::Display for Color {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        fmt::Debug::fmt(self, f)
+    }
+}
+
+fn fancy_print_guess(guess: &[Color]) {
+    let mut vec_colors: Vec<char> = Vec::new();
+
+    for color in guess {
+        vec_colors.push(color.to_string().chars().next().unwrap());
+    }
+    let color_str: String = vec_colors.into_iter().collect();
+    println!("{:?}", color_str);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let guess = vec![
+        Color::Black,
+        Color::White,
+        Color::Black,
+        Color::Green,
+        Color::Blue,
+    ];
+    fancy_print_guess(&[
+        Color::Red,
+        Color::Red,
+        Color::Blue,
+        Color::Yellow,
+        Color::Green,
+    ]);
 }

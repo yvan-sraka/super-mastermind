70acdbd6cd247a7643a0ef8e249dfd257ee0cfc3
itsyanes <touahriyanis@gmail.com>
2021-04-18 15:54:11 +0200

    level 10

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use ansi_term::Colour;
 use std::io;
-use std::fs::File;
-use std::io::{Seek, SeekFrom, Read};
 
 fn main() -> io::Result<()> {
     #[derive(Debug, PartialEq)]
@@ -9,7 +7,6 @@ fn main() -> io::Result<()> {
         Cyan,
         Green,
         Blue,
-        Black,
         Purple,
         Red,
         White,
@@ -22,41 +19,65 @@ fn main() -> io::Result<()> {
                 Color::Yellow => print!("{}", Colour::Yellow.paint("Y")),
                 Color::Cyan => print!("{}", Colour::Cyan.paint("C")),
                 Color::Green => print!("{}", Colour::Green.paint("G")),
-                Color::Black => print!("{}", Colour::Black.paint("M")),
-                Color::Blue => print!("{}", Colour::Blue.paint("O")),
+                Color::Blue => print!("{}", Colour::Blue.paint("B")),
                 Color::Purple => print!("{}", Colour::Purple.paint("P")),
                 Color::Red => print!("{}", Colour::Red.paint("R")),
                 Color::White => print!("{}", Colour::White.paint("W")),
             }
         }
+        println!("");
     }
 
-    fn factory_print_guess(proposal: String){
-
+    fn parse_vec_into_string(guess: &[Color]) -> String {
+        let mut result = String::new();
+        for color in guess {
+            match color {
+                Color::Yellow => result.push('Y'),
+                Color::Cyan => result.push('C'),
+                Color::Green => result.push('G'),
+                Color::Blue => result.push('B'),
+                Color::Purple => result.push('P'),
+                Color::Red => result.push('R'),
+                Color::White => result.push('W'),
+            }
+        }
+        return result;
+    } 
+    
+    fn parse_string_into_vec(input: String) -> Vec<Color>{
+        let mut colors:Vec<Color> = vec![];
+        for aa in input.chars() { 
+            match aa {
+                'Y' => colors.push(Color::Yellow),
+                'C' => colors.push(Color::Cyan),
+                'G' => colors.push(Color::Green),
+                'B' => colors.push(Color::Blue),
+                'P' => colors.push(Color::Purple),
+                'R' => colors.push(Color::Red),
+                'W' => colors.push(Color::White),
+                '\n' => print!("word: "),
+                _ => {
+                    println!("Wrong input");
+                    return vec![];
+                }
+            }
+        }
+        fancy_print_guess(&colors);
+        return colors;
     }
 
-
-    let guess:Vec<Color> = vec![Color::Cyan,Color::Green,Color::Black,Color::Yellow,Color::Purple];
-
-
-
-    let mut input = String::new();
-    io::stdin().read_line(&mut input)?;
-    factory_print_guess(input);
-
-
-    // fancy_print_guess(&guess);
-    // fancy_print_guess(&[Color::Red, Color::Purple, Color::Black, Color::Yellow, Color::Green]);
-
-/*    loop {
-        fancy_print_guess(&guess);
+    let guess:Vec<Color> = vec![Color::Cyan,Color::Green,Color::Blue,Color::Yellow,Color::Purple];
+    let mut nb_tries = 0;
+    loop {
         let mut input = String::new();
         io::stdin().read_line(&mut input)?;
-
-        if input.trim() == "CGBYP"{
-            break;
+        let mut results :Vec<Color> = parse_string_into_vec(input);
+        if results.len() > 0 {
+            nb_tries += 1;
+            println!("number of tries: {}", nb_tries);
         }
-    }*/
-    Ok(())
+    }
 
+
+    Ok(())
 }

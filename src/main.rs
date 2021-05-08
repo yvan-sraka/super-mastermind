112e97aa9d06ce535ce727c90d453755118ca7ca
masatakaIshii <ishiimasataka@yahoo.fr>
2021-05-08 17:43:54 +0200

    Leve 12 done with unit tests of number_of_not_well_placed_pawns function

--- src/main.rs
+++ src/main.rs
@@ -88,6 +88,26 @@ fn get_str_colors_stdin() -> String {
     return s;
 }
 
+fn number_of_not_well_placed_pawns(secret: &[Color], guess: &[Color]) -> i32 {
+    let mut number_not_well_placed = 0;
+    let mut index_not_well_placed = Vec::<usize>::new();
+
+    for (i, _guess_color) in guess.iter().enumerate() {
+        for (j, _secret_color) in secret.iter().enumerate() {
+            if i != j
+                && _guess_color == _secret_color
+                && &secret[i] != _guess_color
+                && !index_not_well_placed.contains(&j)
+            {
+                number_not_well_placed = number_not_well_placed + 1;
+                index_not_well_placed.push(j);
+            }
+        }
+    }
+
+    return number_not_well_placed;
+}
+
 fn number_of_well_placed_pawns(secret: &[Color], guess: &[Color]) -> i32 {
     let mut number_well_placed = 0;
     for (i, _color) in guess.iter().enumerate() {
@@ -108,6 +128,12 @@ fn compare_current_colors_to_secret_colors(
         return true;
     }
     println!("number_well_placed_pawns : {}", number_well_placed_pawns);
+    let number_not_well_placed_pawns =
+        number_of_not_well_placed_pawns(&secret_colors, &current_colors);
+    println!(
+        "number not well placed pawns : {}",
+        number_not_well_placed_pawns
+    );
     for (i, _color) in current_colors.iter().enumerate() {
         if secret_colors[i] != *_color {
             result = false;
@@ -142,4 +168,58 @@ fn start_game(secret_colors: Vec<Color>) {
 fn main() {
     let secret_colors = vec![Color::Blue, Color::Green, Color::White];
     start_game(secret_colors);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::number_of_not_well_placed_pawns;
+    use super::Color;
+
+    #[test]
+    fn not_well_placed_pawns_are_not_well_placed_pawns() {
+        let secret_colors = vec![Color::Blue, Color::Green, Color::White];
+        let guess_colors = vec![Color::Blue, Color::Green, Color::White];
+        let result = number_of_not_well_placed_pawns(&secret_colors, &guess_colors);
+        assert_eq!(0, result);
+    }
+
+    #[test]
+    fn when_guess_colors_contain_one_color_on_secret_colors_but_not_well_placed_should_return_1() {
+        let secret_colors = vec![Color::Blue, Color::Green, Color::White];
+        let guess_colors = vec![Color::Orange, Color::Blue, Color::Yellow];
+        let result = number_of_not_well_placed_pawns(&secret_colors, &guess_colors);
+        assert_eq!(1, result);
+    }
+
+    #[test]
+    fn when_guess_colors_contain_3_colors_on_secret_colors_but_not_well_placed_should_return_3() {
+        let secret_colors = vec![Color::Blue, Color::Green, Color::White];
+        let guess_colors = vec![Color::Green, Color::White, Color::Blue];
+        let result = number_of_not_well_placed_pawns(&secret_colors, &guess_colors);
+        assert_eq!(3, result);
+    }
+
+    #[test]
+    fn when_guess_colors_not_contain_colors_on_secret_colors_should_return_0() {
+        let secret_colors = vec![Color::Blue, Color::Green, Color::White];
+        let guess_colors = vec![Color::Yellow, Color::Purple, Color::Orange];
+        let result = number_of_not_well_placed_pawns(&secret_colors, &guess_colors);
+        assert_eq!(0, result);
+    }
+
+    #[test]
+    fn when_guess_colors_contain_2_same_colors_of_only_one_on_secret_colors_should_return_1() {
+        let secret_colors = vec![Color::Blue, Color::Green, Color::White];
+        let guess_colors = vec![Color::Green, Color::Purple, Color::Green];
+        let result = number_of_not_well_placed_pawns(&secret_colors, &guess_colors);
+        assert_eq!(1, result);
+    }
+
+    #[test]
+    fn when_guess_colors_contain_2_same_colors_of_2_on_secret_colors_should_return_2() {
+        let secret_colors = vec![Color::Blue, Color::Green, Color::White, Color::Green];
+        let guess_colors = vec![Color::Green, Color::Purple, Color::Green, Color::Purple];
+        let result = number_of_not_well_placed_pawns(&secret_colors, &guess_colors);
+        assert_eq!(2, result);
+    }
 }

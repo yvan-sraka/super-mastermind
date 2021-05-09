fa3f39ffb4188b637f5f67fae250e00db3a22f5e
Mustakim-Khan <mkhan@myges.fr>
2021-05-09 17:31:50 +0200

    level 9

--- src/main.rs
+++ src/main.rs
@@ -2,42 +2,47 @@ use ansi_term::Colour;
 use std::io;
 
 fn main() {
-    let mut guess: Vec<Color> = Vec::new();
-    let mut vec: Vec<Color> = Vec::new();
-    guess.push(Color::Red);
-    guess.push(Color::Blue);
-    guess.push(Color::Green);
-    guess.push(Color::Yellow);
-    guess.push(Color::Purple);
-    guess.push(Color::White);
-    guess.push(Color::Cyan);
-    guess.push(Color::Maroon);
+    // let mut vec: Vec<Color> = Vec::new();
+    // guess.push(Color::Red);
+    // guess.push(Color::Blue);
+    // guess.push(Color::Green);
+    // guess.push(Color::Yellow);
+    // guess.push(Color::Purple);
+    // guess.push(Color::White);
+    // guess.push(Color::Cyan);
+    // guess.push(Color::Maroon);
 
     //rintln!("{:?}", Color::Maroon);
 
-    print_vec(guess);
+    // print_vec(guess);
 
     //println!("{}", Colour::RGB(102, 51, 0).paint("M"));
     fancy_print_guess(&[Color::Red, Color::Red, Color::Blue, Color::Yellow, Color::Green, Color::Maroon]);
 
     let mut input = String::new();
     loop{
-        let error = io::stdin().read_line(&mut input);
-        // match input.as_str(){
-        //     "R" => vec.push(Color::Red),
-        //     _ => println!("Got some other input")
-        // }
-        for c in input.chars() {
-            match c{
-                'R' => vec.to_owned().push(Color::Red),
-                _ => println!("{}", c)
-            }
-            //print!("{}", c);
+        let mut guess = vec![];
+        io::stdin().read_line(&mut input);
+        println!("Input : {}", input);
+        let letters = input.trim();
+        for letter in letters.chars(){
+            let color = match letter {
+                'R' => Color::Red,
+                'B' => Color::Blue,
+                'G' => Color::Green,
+                'Y' => Color::Yellow,
+                'P' => Color::Purple,
+                'W' => Color::White,
+                'C' => Color::Cyan,
+                'M' => Color::Maroon,
+                _ => { 
+                    println!("Color {} is invalid", letter);
+                    break;
+                }
+            };
+            guess.push(color);
         }
-        //println!("{}", input);
-        input = String::from("");
-        println!("error: {:?}", error);
-        print_vec(vec);
+        print_vec(guess);
     }
 }
 

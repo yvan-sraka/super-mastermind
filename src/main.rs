4fe7d9457e2d403e3a0df92434d91d552747c0c9
Aristide Bamenou <aristide.bamenou@hotmail.com>
2021-05-09 21:30:19 +0200

    Fix ansi_color

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,48 @@
+use ansi_term::Colour::{Black, White, Yellow, Red, Purple, Blue, Green, Cyan};
+
 #[warn(dead_code)]
 #[derive(Debug)]
 enum Color {
-    Blue,
+    Black,
+    White,
     Yellow,
     Red,
-    Orange,
-    Grey,
-    White,
     Purple,
-    Navy
+    Blue,
+    Green,
+    Cyan
 }
 
 fn fancy_print_guess(guess: &[Color]) {
-    let mut output = String::from("");
+    let mut output = String::new();
+    let mut format_color;
     for color in guess {
-        let format_color = format!("{:?}", color);
-        output.push_str(&format_color[..1]);
+        format_color = format!("{:?}", color);
+        match color {
+            Color::Black => { print!("{}", Black.paint(&format_color[..1])); }
+            Color::White => { print!("{}", White.paint(&format_color[..1])); }
+            Color::Yellow => { print!("{}", Yellow.paint(&format_color[..1])); }
+            Color::Red => { print!("{}", Red.paint(&format_color[..1])); }
+            Color::Purple => { print!("{}", Purple.paint(&format_color[..1])); }
+            Color::Blue => { print!("{}", Blue.paint(&format_color[..1])); }
+            Color::Green => { print!("{}", Green.paint(&format_color[..1])); }
+            Color::Cyan => { print!("{}", Cyan.paint(&format_color[..1])); }
+        }
     }
-    println!("{:?}", output)
+    println!();
 }
 
 fn main() {
-    let guess = [Color::Blue, Color::Yellow, Color::Red, Color::Grey, Color::Purple];
+    let guess = [Color::Blue, Color::Yellow, Color::Red, Color::Green, Color::Purple];
     fancy_print_guess(&guess);
-    reading_and_printing_loop();
+    // reading_and_printing_loop();
 }
 
-fn reading_and_printing_loop() {
+fn reading_and_printing_loop(guess :&[Color]) {
     loop {
         let mut line =String::new();
         std::io::stdin().read_line(&mut line);
+
         println!("{}", line);
     }
 }

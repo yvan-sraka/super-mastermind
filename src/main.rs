43db7084f3f4a365133795bc117038a798510926
Aristide Bamenou <aristide.bamenou@hotmail.com>
2021-05-09 17:07:12 +0200

    level 6
    # Conflicts:
    #	Cargo.toml
    #	src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+#[derive(Debug)]
+enum Color {
+    Blue,
+    Yellow,
+    Red,
+    Orange,
+    Grey,
+    White,
+    Purple,
+    Navy
+}
+
+fn fancy_print_guess(guess: &[Color]) {
+    let mut output = String::from("");
+    for color in guess {
+        let format_color = format!("{:?}", color);
+        output.push_str(&format_color[..1]);
+    }
+    println!("{:?}", output)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let guess = [Color::Blue, Color::Yellow, Color::Red, Color::Grey, Color::Purple];
+    fancy_print_guess(&guess);
 }

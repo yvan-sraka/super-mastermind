2c347a72e4a2559112e4b9104e6ed66289e3cd38
barder60 <kaderate.yamamoto@hotmail.fr>
2021-05-09 21:42:19 +0200

    exo(main): level 6

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 #[derive(Debug)]
 enum Color {
     Red,
-    Pink,
-    Blue,
+    Orange,
+    Cyan,
     Black,
     Green,
     White,
@@ -10,13 +10,30 @@ enum Color {
     Yellow
 }
 
+fn fancy_print_guess(guess: &[Color]) {
+    for color in guess {
+        match color {
+            Color::Red => print!("R"),
+            Color::Orange => print!("O"),
+            Color::Cyan => print!("C"),
+            Color::Black => print!("B"),
+            Color::Green => print!("G"),
+            Color::White => print!("W"),
+            Color::Purple => print!("P"),
+            Color::Yellow => print!("Y"),
+            _ => println!("Color not found"),
+        }
+    }
+    println!("");
+}
+
 fn main() {
     let mut guess: Vec<Color> = Vec::new();
     guess.push(Color::Black);
-    guess.push(Color::Blue);
+    guess.push(Color::Orange);
     guess.push(Color::Green);
-    guess.push(Color::Black);
+    guess.push(Color::Cyan);
     guess.push(Color::Black);
 
-    println!("`{:?}`", guess);
+    fancy_print_guess(&guess);
 }
